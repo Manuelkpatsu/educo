5d24f9d6fd4cdb78f67272c6c4fa3893aa954c2b
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-06-20 23:47:23 +0000

    Add custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'theme/custom_theme.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,10 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: CustomTheme.theme,
       home: const MyHomePage(),
     );
   }
---
+++ lib/theme/custom_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart' show Color;
+
+class CustomColor {
+  static const Color primaryColor = Color(0xff265ae8);
+  static const Color appBarIconColor = Color(0xff292d32);
+  static const Color appBarTitleColor = Color(0xff0b121f);
+  static const Color blackColor = Color(0xff000000);
+}
